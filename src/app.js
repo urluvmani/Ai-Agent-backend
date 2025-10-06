590300e4576dac34efc4de2d79dc4549cf3ea283
Hafiz Muhammad Abdul Rehman <abdulrehman90190@gmail.com>
2025-10-06 14:44:44 -0700

    initial commit

--- src/app.js
+++ src/app.js
@@ -4,13 +4,16 @@ const router = require("./routes/ai.route");
 const app = express();
 
 app.use(
-  cors({
-    origin: ["https://ai-mani.vercel.app"], // your frontend domain
+    cors({
+    origin: ["https://ai-mani.vercel.app","http://localhost:5173"], // your frontend domain
     methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
+app.options(/.*/, cors());
+
+
 
 app.use(express.json());
 app.use("/ai", router);
